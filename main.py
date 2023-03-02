8d2da654675aa12af53ff686b8906a4282cddf30
Marion <mrobert148@gmail.com>
2023-03-02 11:56:21 +0100

    create dictionnary from csv file using pandas and dictionary comprehension

--- main.py
+++ main.py
@@ -3,25 +3,30 @@ student_dict = {
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     print(key)
+#     print(value)
+
 
 import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     print(index)
+#     print(row)
+#     if row.student == "Angela":
+#         print(row.score)
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+dictionary = {row.letter:row.code for (index, row) in data.iterrows()}
+print(dictionary)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 

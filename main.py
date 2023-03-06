4c30c74b5a52f47d63138c90dc63d05baa68e1e6
Marion <mrobert148@gmail.com>
2023-03-06 10:36:43 +0100

    Angela's answer : using function instead of while loop

--- main.py
+++ main.py
@@ -44,3 +44,15 @@ phonetic_dictionary = {row.letter: row.code for (index, row) in data.iterrows()}
 #         print(output_list)
 
 
+# Angela's solution with creation of a function
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dictionary[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
